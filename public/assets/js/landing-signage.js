7006112e8e8005af86c130cde811f1aa2edb26c4
sirexlaangnmn <potolin.federex@gmail.com>
2023-09-27 19:48:51 +0800

    Landinge page update: signages and graphics

--- public/assets/js/landing-signage.js
+++ public/assets/js/landing-signage.js
@@ -1,82 +1,303 @@
-// Define an array of objects containing name and image source
-const signageCard = [
+const signageCardData = [
   {
     name: "Acrylic Signage",
-    imageSrc: "uploads/signage/acrylic_signage.jpg"
+    imageSrc: "uploads/signage/acrylic_signage.jpg",
+    color: 'black',
   },
   {
     name: "Advertising",
-    imageSrc: "uploads/signage/advertising.jpg"
+    imageSrc: "uploads/signage/advertising.jpg",
+    color: 'black',
   },
   {
     name: "Billboards",
-    imageSrc: "uploads/signage/billboards.jpg"
+    imageSrc: "uploads/signage/billboards.jpg",
+    color: 'white',
   },
   {
     name: "Digital Signage",
-    imageSrc: "uploads/signage/digital_signage.jpg"
+    imageSrc: "uploads/signage/digital_signage.jpg",
+    color: 'black',
   },
   {
     name: "Embroidery",
-    imageSrc: "uploads/signage/embroidery.jpg"
+    imageSrc: "uploads/signage/embroidery.jpg",
+    color: 'black',
   },
   {
     name: "Etching, Engraving & Carving",
-    imageSrc: "uploads/signage/EtchingEngravingCarving.jpg"
+    imageSrc: "uploads/signage/EtchingEngravingCarving.jpg",
+    color: 'white',
   },
   {
     name: "Glass Signage",
-    imageSrc: "uploads/signage/glass_signage.jpg"
+    imageSrc: "uploads/signage/glass_signage.jpg",
+    color: 'black',
   },
   {
     name: "LEDs / LED Walls",
-    imageSrc: "uploads/signage/LED_walls.jpg"
+    imageSrc: "uploads/signage/LED_walls.jpg",
+    color: 'white',
   },
   {
     name: "Metal Signage",
-    imageSrc: "uploads/signage/metal_signage.jpg"
+    imageSrc: "uploads/signage/metal_signage.jpg",
+    color: 'black',
   },
   {
     name: "Neon Sign",
-    imageSrc: "uploads/signage/neon_sign.jpg"
+    imageSrc: "uploads/signage/neon_sign.jpg",
+    color: 'white',
   },
   {
     name: "Outdoor Signage",
-    imageSrc: "uploads/signage/outdoor_signage.jpg"
+    imageSrc: "uploads/signage/outdoor_signage.jpg",
+    color: 'white',
   },
   {
     name: "Panaflex Signage",
-    imageSrc: "uploads/signage/panaflex_signage.jpg"
+    imageSrc: "uploads/signage/panaflex_signage.jpg",
+    color: 'white',
   },
   {
     name: "Promotional Products",
-    imageSrc: "uploads/signage/promotional_products.jpg"
+    imageSrc: "uploads/signage/promotional_products.jpg",
+    color: 'yellow-400',
   },
   {
     name: "Retail Signage",
-    imageSrc: "uploads/signage/retail_signage.jpg"
+    imageSrc: "uploads/signage/retail_signage.jpg",
+    color: 'black',
   },
   {
     name: "Signage Supplies",
-    imageSrc: "uploads/signage/signage_supplies.jpg"
+    imageSrc: "uploads/signage/signage_supplies.jpg",
+    color: 'black',
+  },
+  {
+    name: "3D Printing",
+    imageSrc: "uploads/signage/3D_printing.jpg",
+    color: 'white',
+  },
+  {
+    name: "Apparel Printing",
+    imageSrc: "uploads/signage/apparel_printing.jpg",
+    color: 'black',
+  },
+  {
+    name: "Banner Printing",
+    imageSrc: "uploads/signage/banner_printing.jpg",
+    color: 'black',
+  },
+  {
+    name: "Corporate Giveaways",
+    imageSrc: "uploads/signage/corporate_giveaways.jpg",
+    color: 'black',
+  },
+  {
+    name: "Graphic Design Studios",
+    imageSrc: "uploads/signage/graphic_design_Studios.jpg",
+    color: 'black',
+  },
+  {
+    name: "Laser Printing / Cutting",
+    imageSrc: "uploads/signage/laser_printing_cutting.jpg",
+    color: 'black',
+  },
+  {
+    name: "Logo Designers",
+    imageSrc: "uploads/signage/logo_designers.jpg",
+    color: 'white',
+  },
+  {
+    name: "Packaging Design and Printing",
+    imageSrc: "uploads/signage/packaging_design_and_printing.jpg",
+    color: 'black',
+  },
+  {
+    name: "Personalization",
+    imageSrc: "uploads/signage/personalization.jpg",
+    color: 'black',
+  },
+  {
+    name: "Photography Studios",
+    imageSrc: "uploads/signage/photography_studios.jpg",
+    color: 'white',
+  },
+  {
+    name: "Print Finishing and Binding",
+    imageSrc: "uploads/signage/print_finishing_and_binding.jpg",
+    color: 'black',
+  },
+  {
+    name: "Printers",
+    imageSrc: "uploads/signage/printers.jpg",
+    color: 'black',
+  },
+  {
+    name: "Promotional Video Production",
+    imageSrc: "uploads/signage/promotional_video_production.jpg",
+    color: 'white',
+  },
+  {
+    name: "Sticker Printing",
+    imageSrc: "uploads/signage/sticker_printing_companies.jpg",
+    color: 'white',
+  },
+  {
+    name: "Vehicle Wrap and Graphics",
+    imageSrc: "uploads/signage/vehicle_wrap_and_graphics_companies.jpg",
+    color: 'black',
+  },
+  {
+    name: "Vinyl Decal and Lettering Services",
+    imageSrc: "uploads/signage/vinyl_decal_and_lettering_services.jpg",
+    color: 'black',
+  },
+  {
+    name: "Wall Decal and Mural Design",
+    imageSrc: "uploads/signage/wall_decal_and_mural_design.jpg",
+    color: 'white',
+  },
+  {
+    name: "Web Design and Development",
+    imageSrc: "uploads/signage/web_design_and_development.jpg",
+    color: 'black',
+  },
+  {
+    name: "Wide Format Printing ",
+    imageSrc: "uploads/signage/wide_format_printing.jpg",
+    color: 'black',
   },
 ];
 
-// Get a reference to the container where you want to append the cards
-const container = document.getElementById("signageCardContainer");
+const signageCardContainer = document.getElementById("signageCardContainer");
 
-// Loop through the signageCard array and generate HTML cards
-signageCard.forEach((item) => {
+signageCardData.sort((a, b) => a.name.localeCompare(b.name));
+
+signageCardData.forEach((item) => {
   const signageCardHtml = `
     <div class="card">
       <div class="relative w-full h-56 overflow-hidden rounded-md">
         <div class="absolute z-10 w-full h-3/4 -bottom-12 bg-gradient-to-b from-transparent to-gray-800"></div>
         <img src="${item.imageSrc}" class="absolute object-cover w-full h-full" alt="">
-        <div class="absolute bottom-0 z-20 w-full p-3 text-lg font-semibold text-black hover:text-blue-900">${item.name}</div>
+        <div class="absolute bottom-0 z-20 w-full p-3 text-lg font-semibold text-${item.color}">
+          ${item.name}
+        </div>
       </div>
     </div>
   `;
 
-  // Append the card HTML to the container
-  container.insertAdjacentHTML('beforeend', signageCardHtml);
+  signageCardContainer.insertAdjacentHTML('beforeend', signageCardHtml);
+});
+
+
+
+const tradersVideoData = [
+  {
+    title: "Matrix Creation Events Venue",
+    text: 'Multiple Virtual Reality Scenery on 3 Story High Video Wall at Matrix Creation Events Venue',
+    imageSrc: 'uploads/traders-videos-thumbnail/matrix-creation-events-venue-300.webp',
+    imageAlt: 'Matrix Creation Events Venue Traders Video | All World Trade',
+    youtubeLink: 'https://www.youtube.com/watch?v=hwy7K7FJnZ4',
+    duration: '12:21',
+  },
+  {
+    title: 'Team One Vision',
+    text: 'Pru Life UK continues to make it their priority to help Filipinos achieve financial stability.',
+    imageSrc: 'uploads/traders-videos-thumbnail/team-one-vision-300.webp',
+    imageAlt: 'Team One Vision Traders Video | All World Trade',
+    youtubeLink: 'https://www.youtube.com/watch?v=ICvW1755Puc',
+    duration: '02:00',
+  },
+  {
+    title: 'The Scent Connection CEO',
+    text: 'Meet the Scent Connection CEO from the 3rd generation of perfumery business tycoons',
+    imageSrc: 'uploads/traders-videos-thumbnail/scent-300.webp',
+    imageAlt: 'Scent Traders Video | All World Trade',
+    youtubeLink: 'https://www.youtube.com/watch?v=BRHRAgIeUlE',
+    duration: '01:22',
+  },
+  {
+    title: 'Ionspec MEDICAL EYEWEAR Testimony',
+    text: 'GLAUCOMA PATIENT 99 % NO VISION, sa Ionspec NAKA-ANINAG ULET',
+    imageSrc: 'uploads/traders-videos-thumbnail/mgi-ionspec-medical-eye-wear-300.webp',
+    imageAlt: 'MGI Ionspec Medical Eye Wear Traders Video | All World Trade',
+    youtubeLink: 'https://www.facebook.com/watch/?v=573257374604592',
+    duration: '06:38',
+  },
+  {
+    title: 'How MGI Medical Eyewear Helps our Eyes!',
+    text: 'Our MGI Eyewear simple explanation of how it can help our eyes!',
+    imageSrc: 'uploads/traders-videos-thumbnail/mgi-ionspec-medical-eye-wear-300.webp',
+    imageAlt: 'MGI Ionspec Medical Eye Wear Traders Video | All World Trade',
+    youtubeLink: 'https://www.facebook.com/mgiproductsphilippines/videos/763856057547376/',
+    duration: '11:55',
+  },
+  {
+    title: 'Mystic Skin',
+    text: 'Sesh #tattoo #CarcarCitytattoo #cebuphilippines #mysticskintattoo #sgoonstattoosupply #southsidecommunity📱0 9 5 6 1 2 9 6 5 0 0',
+    imageSrc: 'uploads/traders-videos-thumbnail/mystic-skin-300.webp',
+    imageAlt: 'Mystic Skin Traders Video | All World Trade',
+    youtubeLink: 'https://www.facebook.com/watch/?v=680867256774296&ref=sharing&_rdc=1&_rdr',
+    duration: '0:51',
+  },
+  {
+    title: 'Ding-Adeth KALAW Pili Pastries',
+    text: 'A family business founded by my grandfather in the 70’s. Engaged in trading buying and selling',
+    imageSrc: 'uploads/traders-videos-thumbnail/ding_adeth_shop-300.webp',
+    imageAlt: 'Ding Adeth Traders Video | All World Trade',
+    youtubeLink: 'https://www.facebook.com/watch/?v=1963204720660328&ref=sharing&_rdc=1&_rdr',
+    duration: '0:53',
+  },
+  {
+    title: 'Imperial Royalty Pomeranian',
+    text: '#pomeranian #philippines #whitepomeranian philippines whitepomeranian Pomeranian Philippines',
+    imageSrc: 'uploads/traders-videos-thumbnail/pomeranian-300.webp',
+    imageAlt: 'Pomeranian Traders Video | All World Trade',
+    youtubeLink: 'https://www.facebook.com/100080231707449/videos/1316147259241076/',
+    duration: '2:22',
+  },
+];
+
+const tradersVideoContainer = document.getElementById("tradersVideoContainer");
+
+tradersVideoData.forEach((item) => {
+  const tradersVideoHtml = `
+    <li class="mb-10">
+      <a href="${item.youtubeLink}"
+          class="relative block w-full overflow-hidden rounded-lg md:h-44 h-28"
+          target="_blank">
+      <img src="${item.imageSrc}"
+          alt="${item.imageAlt}" class="absolute inset-0 object-cover w-full h-full" />
+      <span class="
+          absolute
+          bg-black bg-opacity-60
+          bottom-1
+          font-semibold
+          px-1.5
+          py-0.5
+          right-1
+          rounded
+          text-white text-xs
+          ">
+          ${item.duration}
+          </span>
+      <img src="assets/images/icon-play.svg"
+          class="w-12 h-12 uk-position-center" alt="${item.imageAlt}" />
+      </a>
+      <div class="p-5 pt-3">
+          <a href="${item.youtubeLink}"
+              class="font-semibold line-clamp-2 text-lg" target="_blank">
+            ${item.title}
+          </a>
+          <div class="pt-4">
+              <a href="${item.youtubeLink}"
+                  class="text-sm" target="_blank"> ${item.text} </a>
+          </div>
+      </div>
+  </li>
+  `;
+
+  tradersVideoContainer.insertAdjacentHTML('beforeend', tradersVideoHtml);
 });
