f34daa5182d80cf612881dfcff5848a0db0ed30e
sirexlaangnmn <potolin.federex@gmail.com>
2024-01-29 23:07:39 +0800

    all about events

--- public/assets/js/landing-signage.js
+++ public/assets/js/landing-signage.js
@@ -1,175 +1,427 @@
+// const signageCardData = [
+//   {
+//     name: "Acrylic Signage",
+//     imageSrc: "uploads/signage/acrylic_signage.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Advertising",
+//     imageSrc: "uploads/signage/advertising.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Billboards",
+//     imageSrc: "uploads/signage/billboards.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Digital Signage",
+//     imageSrc: "uploads/signage/digital_signage.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Embroidery",
+//     imageSrc: "uploads/signage/embroidery.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Etching, Engraving & Carving",
+//     imageSrc: "uploads/signage/EtchingEngravingCarving.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Glass Signage",
+//     imageSrc: "uploads/signage/glass_signage.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "LEDs / LED Walls",
+//     imageSrc: "uploads/signage/LED_walls.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Metal Signage",
+//     imageSrc: "uploads/signage/metal_signage.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Neon Sign",
+//     imageSrc: "uploads/signage/neon_sign.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Outdoor Signage",
+//     imageSrc: "uploads/signage/outdoor_signage.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Panaflex Signage",
+//     imageSrc: "uploads/signage/panaflex_signage.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Promotional Products",
+//     imageSrc: "uploads/signage/promotional_products.jpg",
+//     color: 'yellow-400',
+//   },
+//   {
+//     name: "Retail Signage",
+//     imageSrc: "uploads/signage/retail_signage.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Signage Supplies",
+//     imageSrc: "uploads/signage/signage_supplies.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "3D Printing",
+//     imageSrc: "uploads/signage/3D_printing.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Apparel Printing",
+//     imageSrc: "uploads/signage/apparel_printing.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Banner Printing",
+//     imageSrc: "uploads/signage/banner_printing.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Corporate Giveaways",
+//     imageSrc: "uploads/signage/corporate_giveaways.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Graphic Design Studios",
+//     imageSrc: "uploads/signage/graphic_design_Studios.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Laser Printing / Cutting",
+//     imageSrc: "uploads/signage/laser_printing_cutting.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Logo Designers",
+//     imageSrc: "uploads/signage/logo_designers.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Packaging Design and Printing",
+//     imageSrc: "uploads/signage/packaging_design_and_printing.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Personalization",
+//     imageSrc: "uploads/signage/personalization.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Photography Studios",
+//     imageSrc: "uploads/signage/photography_studios.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Print Finishing and Binding",
+//     imageSrc: "uploads/signage/print_finishing_and_binding.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Printers",
+//     imageSrc: "uploads/signage/printers.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Promotional Video Production",
+//     imageSrc: "uploads/signage/promotional_video_production.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Sticker Printing",
+//     imageSrc: "uploads/signage/sticker_printing_companies.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Vehicle Wrap and Graphics",
+//     imageSrc: "uploads/signage/vehicle_wrap_and_graphics_companies.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Vinyl Decal and Lettering Services",
+//     imageSrc: "uploads/signage/vinyl_decal_and_lettering_services.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Wall Decal and Mural Design",
+//     imageSrc: "uploads/signage/wall_decal_and_mural_design.jpg",
+//     color: 'white',
+//   },
+//   {
+//     name: "Web Design and Development",
+//     imageSrc: "uploads/signage/web_design_and_development.jpg",
+//     color: 'black',
+//   },
+//   {
+//     name: "Wide Format Printing ",
+//     imageSrc: "uploads/signage/wide_format_printing.jpg",
+//     color: 'black',
+//   },
+// ];
+
+
+
 const signageCardData = [
   {
-    name: "Acrylic Signage",
-    imageSrc: "uploads/signage/acrylic_signage.jpg",
+    name: "Audio-Visual Services",
+    imageSrc: "uploads/all-about-events/audio-visual-services.jpg",
     color: 'black',
   },
   {
-    name: "Advertising",
-    imageSrc: "uploads/signage/advertising.jpg",
+    name: "Beverage & Liquor",
+    imageSrc: "uploads/all-about-events/beverage-and-liquor.jpg",
+    color: 'white',
+  },
+  {
+    name: "Bridal Shower Items",
+    imageSrc: "uploads/all-about-events/bridal-shower-items.jpg",
     color: 'black',
   },
   {
-    name: "Billboards",
-    imageSrc: "uploads/signage/billboards.jpg",
+    name: "Cakes",
+    imageSrc: "uploads/all-about-events/cakes.jpg",
     color: 'white',
   },
   {
-    name: "Digital Signage",
-    imageSrc: "uploads/signage/digital_signage.jpg",
+    name: "Car Rentals",
+    imageSrc: "uploads/all-about-events/car-rentals.jpg",
     color: 'black',
   },
   {
-    name: "Embroidery",
-    imageSrc: "uploads/signage/embroidery.jpg",
+    name: "Caterers",
+    imageSrc: "uploads/all-about-events/caterers.jpg",
+    color: 'white',
+  },
+  {
+    name: "Corporate Team Building Services",
+    imageSrc: "uploads/all-about-events/corporate-team-building-services.jpg",
     color: 'black',
   },
   {
-    name: "Etching, Engraving & Carving",
-    imageSrc: "uploads/signage/EtchingEngravingCarving.jpg",
+    name: "Couturiers and Shoes",
+    imageSrc: "uploads/all-about-events/couturiers-and-shoes.jpg",
     color: 'white',
   },
   {
-    name: "Glass Signage",
-    imageSrc: "uploads/signage/glass_signage.jpg",
-    color: 'black',
-  },
-  {
-    name: "LEDs / LED Walls",
-    imageSrc: "uploads/signage/LED_walls.jpg",
+    name: "Crowd Control Services",
+    imageSrc: "uploads/all-about-events/crowd-control-services.jpg",
     color: 'white',
   },
   {
-    name: "Metal Signage",
-    imageSrc: "uploads/signage/metal_signage.jpg",
-    color: 'black',
-  },
-  {
-    name: "Neon Sign",
-    imageSrc: "uploads/signage/neon_sign.jpg",
+    name: "Dessert Suppliers",
+    imageSrc: "uploads/all-about-events/dessert-suppliers.jpg",
     color: 'white',
   },
   {
-    name: "Outdoor Signage",
-    imageSrc: "uploads/signage/outdoor_signage.jpg",
-    color: 'white',
-  },
-  {
-    name: "Panaflex Signage",
-    imageSrc: "uploads/signage/panaflex_signage.jpg",
-    color: 'white',
-  },
-  {
-    name: "Promotional Products",
-    imageSrc: "uploads/signage/promotional_products.jpg",
+    name: "Desserts, Coffee, and Mobile Bars",
+    imageSrc: "uploads/all-about-events/desserts-coffee-and-mobile-bars.jpg",
     color: 'yellow-400',
   },
   {
-    name: "Retail Signage",
-    imageSrc: "uploads/signage/retail_signage.jpg",
+    name: "Entertainment Agencies",
+    imageSrc: "uploads/all-about-events/entertainment-agencies.jpg",
     color: 'black',
   },
   {
-    name: "Signage Supplies",
-    imageSrc: "uploads/signage/signage_supplies.jpg",
+    name: "Event Cleaning Services",
+    imageSrc: "uploads/all-about-events/event-cleaning-services.jpg",
     color: 'black',
   },
   {
-    name: "3D Printing",
-    imageSrc: "uploads/signage/3D_printing.jpg",
+    name: "Event Drone Services",
+    imageSrc: "uploads/all-about-events/event-drone-services.jpg",
     color: 'white',
   },
   {
-    name: "Apparel Printing",
-    imageSrc: "uploads/signage/apparel_printing.jpg",
+    name: "Event Rental Services",
+    imageSrc: "uploads/all-about-events/event-rental-services.jpg",
     color: 'black',
   },
   {
-    name: "Banner Printing",
-    imageSrc: "uploads/signage/banner_printing.jpg",
+    name: "Event Security Services",
+    imageSrc: "uploads/all-about-events/event-security-services.jpg",
     color: 'black',
   },
   {
-    name: "Corporate Giveaways",
-    imageSrc: "uploads/signage/corporate_giveaways.jpg",
+    name: "Event Technology Providers",
+    imageSrc: "uploads/all-about-events/event-technology-providers.jpg",
     color: 'black',
   },
   {
-    name: "Graphic Design Studios",
-    imageSrc: "uploads/signage/graphic_design_Studios.jpg",
+    name: "Exhibition Design",
+    imageSrc: "uploads/all-about-events/exhibition-design-and-build-companies.jpg",
     color: 'black',
   },
   {
-    name: "Laser Printing / Cutting",
-    imageSrc: "uploads/signage/laser_printing_cutting.jpg",
+    name: "Financial Services",
+    imageSrc: "uploads/all-about-events/financial-services.jpg",
     color: 'black',
   },
   {
-    name: "Logo Designers",
-    imageSrc: "uploads/signage/logo_designers.jpg",
+    name: "Fireworks / Special Effects",
+    imageSrc: "uploads/all-about-events/fireworks-and-special-effects.jpg",
+    color: 'black',
+  },
+  {
+    name: "Florist",
+    imageSrc: "uploads/all-about-events/florist.jpg",
     color: 'white',
   },
   {
-    name: "Packaging Design and Printing",
-    imageSrc: "uploads/signage/packaging_design_and_printing.jpg",
+    name: "Food Catering",
+    imageSrc: "uploads/all-about-events/food-catering.jpg",
     color: 'black',
   },
   {
-    name: "Personalization",
-    imageSrc: "uploads/signage/personalization.jpg",
+    name: "Giveaways and Souvenirs",
+    imageSrc: "uploads/all-about-events/giveaways-and-souvenirs.jpg",
     color: 'black',
   },
   {
-    name: "Photography Studios",
-    imageSrc: "uploads/signage/photography_studios.jpg",
+    name: "Gown and Suit Designer",
+    imageSrc: "uploads/all-about-events/gown-and-suit-designer.jpg",
     color: 'white',
   },
   {
-    name: "Print Finishing and Binding",
-    imageSrc: "uploads/signage/print_finishing_and_binding.jpg",
+    name: "Specialty Food Carts",
+    imageSrc: "uploads/all-about-events/grazing-table-and-specialty-food-carts.jpg",
     color: 'black',
   },
   {
-    name: "Printers",
-    imageSrc: "uploads/signage/printers.jpg",
+    name: "Hair and Make Up",
+    imageSrc: "uploads/all-about-events/hair-and-make-up.jpg",
     color: 'black',
   },
   {
-    name: "Promotional Video Production",
-    imageSrc: "uploads/signage/promotional_video_production.jpg",
+    name: "Honeymoon Destinations",
+    imageSrc: "uploads/all-about-events/honeymoon-destinations.jpg",
     color: 'white',
   },
   {
-    name: "Sticker Printing",
-    imageSrc: "uploads/signage/sticker_printing_companies.jpg",
+    name: "Hosts",
+    imageSrc: "uploads/all-about-events/hosts.jpg",
+    color: 'black',
+  },
+  {
+    name: "hotels",
+    imageSrc: "uploads/all-about-events/hotels.jpg",
     color: 'white',
   },
   {
-    name: "Vehicle Wrap and Graphics",
-    imageSrc: "uploads/signage/vehicle_wrap_and_graphics_companies.jpg",
+    name: "Inflatables",
+    imageSrc: "uploads/all-about-events/inflatables.jpg",
     color: 'black',
   },
   {
-    name: "Vinyl Decal and Lettering Services",
-    imageSrc: "uploads/signage/vinyl_decal_and_lettering_services.jpg",
+    name: "Invitations & Stationery",
+    imageSrc: "uploads/all-about-events/invitations-and-stationery.jpg",
     color: 'black',
   },
   {
-    name: "Wall Decal and Mural Design",
-    imageSrc: "uploads/signage/wall_decal_and_mural_design.jpg",
+    name: "JEWELRIES",
+    imageSrc: "uploads/all-about-events/jewelries.jpg",
     color: 'white',
   },
   {
-    name: "Web Design and Development",
-    imageSrc: "uploads/signage/web_design_and_development.jpg",
+    name: "Lights & Sounds",
+    imageSrc: "uploads/all-about-events/lights-and-sounds.jpg",
     color: 'black',
   },
   {
-    name: "Wide Format Printing ",
-    imageSrc: "uploads/signage/wide_format_printing.jpg",
+    name: "Meeting and Conference",
+    imageSrc: "uploads/all-about-events/meeting-and-conference.jpg",
+    color: 'white',
+  },
+  {
+    name: "Men's Trousseau",
+    imageSrc: "uploads/all-about-events/mens-trousseau.jpg",
+    color: 'white',
+  },
+  {
+    name: "Mobile Bar",
+    imageSrc: "uploads/all-about-events/mobile-bar.jpg",
+    color: 'black',
+  },
+  {
+    name: "Musicians",
+    imageSrc: "uploads/all-about-events/musicians.jpg",
+    color: 'black',
+  },
+  {
+    name: "PHOTO & VIDEO",
+    imageSrc: "uploads/all-about-events/photo-and-video.jpg",
+    color: 'white',
+  },
+  {
+    name: "PHOTO BOOTH",
+    imageSrc: "uploads/all-about-events/photo-booth.jpg",
+    color: 'black',
+  },
+  {
+    name: "Planner & Coordinators",
+    imageSrc: "uploads/all-about-events/planner-and-coordinators.jpg",
+    color: 'black',
+  },
+  {
+    name: "Real Estate",
+    imageSrc: "uploads/all-about-events/real-estate.jpg",
+    color: 'black',
+  },
+  {
+    name: "Social Media Event Promotion Services",
+    imageSrc: "uploads/all-about-events/social-media-event-promotion-services.jpg",
+    color: 'black',
+  },
+  {
+    name: "Stage and Set Design Companies",
+    imageSrc: "uploads/all-about-events/stage-and-set-design-companies.jpg",
+    color: 'black',
+  },
+  {
+    name: "STYLIST",
+    imageSrc: "uploads/all-about-events/stylist.jpg",
+    color: 'black',
+  },
+  {
+    name: "Transportation Services",
+    imageSrc: "uploads/all-about-events/transportation-services.jpg",
+    color: 'black',
+  },
+  {
+    name: "Travel Service",
+    imageSrc: "uploads/all-about-events/travel-service.jpg",
+    color: 'black',
+  },
+  {
+    name: "VENUES",
+    imageSrc: "uploads/all-about-events/venues.jpg",
+    color: 'black',
+  },
+  {
+    name: "Wedding Accessories",
+    imageSrc: "uploads/all-about-events/wedding-accessories.jpg",
+    color: 'black',
+  },
+  {
+    name: "Wedding Apparel & Wardrobes",
+    imageSrc: "uploads/all-about-events/wedding-apparel-and-wardrobes.jpg",
     color: 'black',
   },
 ];
+
+
 
 const signageCardContainer = document.getElementById("signageCardContainer");
 
@@ -178,18 +430,35 @@ signageCardData.sort((a, b) => a.name.localeCompare(b.name));
 signageCardData.forEach((item) => {
   const signageCardHtml = `
     <div class="card">
-      <div class="relative w-full h-56 overflow-hidden rounded-md">
-        <div class="absolute z-10 w-full h-3/4 -bottom-12 bg-gradient-to-b from-transparent to-gray-800"></div>
-        <img src="${item.imageSrc}" class="absolute object-cover w-full h-full" alt="">
-        <div class="absolute bottom-0 z-20 w-full p-3 text-lg font-semibold text-${item.color}">
-          ${item.name}
+      <a href="https://allworldtrade.com/registration" target="_blank">
+        <div class="relative w-full h-56 overflow-hidden rounded-md">
+          <div class="absolute z-10 w-full h-3/4 -bottom-12 bg-gradient-to-b from-transparent to-gray-800"></div>
+          <img src="${item.imageSrc}" class="absolute object-cover w-full h-full" alt="">
         </div>
-      </div>
+        <p class="py-2 text-lg font-semibold text-white text-center">${item.name}</p>
+      </a>
     </div>
   `;
 
   signageCardContainer.insertAdjacentHTML('beforeend', signageCardHtml);
 });
+
+
+// signageCardData.forEach((item) => {
+//   const signageCardHtml = `
+//     <div class="card">
+//       <div class="relative w-full h-56 overflow-hidden rounded-md">
+//         <div class="absolute z-10 w-full h-3/4 -bottom-12 bg-gradient-to-b from-transparent to-gray-800"></div>
+//         <img src="${item.imageSrc}" class="absolute object-cover w-full h-full" alt="">
+//         <div class="absolute bottom-0 z-20 w-full p-3 text-lg font-semibold text-${item.color}">
+//           ${item.name}
+//         </div>
+//       </div>
+//     </div>
+//   `;
+//
+//   signageCardContainer.insertAdjacentHTML('beforeend', signageCardHtml);
+// });
 
 
 
